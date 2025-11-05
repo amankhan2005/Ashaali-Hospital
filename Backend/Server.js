860a8892116458e828112824d752144d2ad042dd
amankhan2005 <amankhan099k@gmail.com>
2025-11-05 19:35:08 +0530

    backend update

--- Backend/Server.js
+++ Backend/Server.js
@@ -108,6 +108,7 @@ const PORT = process.env.PORT || 3000;
 // -------------------- Middleware --------------------
 const allowedOrigins = [
   "http://localhost:5173",
+  "https://www.ashaalihospital.com",
   "https://ashaali-hospital-lko.netlify.app",
   "http://localhost:5174",
   "https://ashaali-adminpage.netlify.app"
